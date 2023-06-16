7bbff8e608e27b9937bb23831afd4784529cd27e
rcodina <rosa.codina@soprasteria.com>
2023-06-16 09:31:41 +0200

    MultipleCustomHook done and barrel file done

--- src/03-examples/MultipleCustomHooks.jsx
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -1,8 +1,9 @@
-import { useFetch } from "../hooks/useFetch";
+import { useCounter, useFetch } from "../hooks";
+import { LoadingQuote, Quote } from "./";
 
 export const MultipleCustomHooks = () => {
-
-  const {data, isLoading, hasError} = useFetch("https://api.breakingbadquotes.xyz/v1/quotes/");
+  const { counter, increment } = useCounter(1);
+  const { data, isLoading, hasError } = useFetch(`https://api.breakingbadquotes.xyz/v1/quotes/${counter}`);
   const { author, quote } = !!data && data[0];
 
   return (
@@ -11,18 +12,15 @@ export const MultipleCustomHooks = () => {
 
       {
         isLoading
-          ? (
-            <div className="alert alert-info text-center">Loading...</div>
-          )
-          : (
-            <blockquote className="blockquote text-end">
-            <p className="mb-1">{ quote }</p>
-            <footer className="blockquote-footer">{ author }</footer>
-            </blockquote>
-          )
+          ? <LoadingQuote />
+          : <Quote author={ author } quote={ quote }/>
       }
 
-      <button className="btn btn-primary">Next quote</button>
+      <button 
+        onClick={ () => increment() }
+        className="btn btn-primary">
+        Next quote
+      </button>
 
     </>
   )
